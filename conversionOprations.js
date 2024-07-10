cb0e7437d4da26bb54f555b3f20e97568d0aa5af
om <om@venturingdigitally.com>
2024-07-10 17:25:28 +0530

    dataTypes Information

---
+++ conversionOprations.js
@@ -0,0 +1,64 @@
+// let score = "33abc";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = null;
+// console.log(typeof score);
+// console.log(score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = undefined;
+// console.log(typeof score);
+// console.log(score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = true;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = false;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+// true => 1
+//false => 0
+//"33abc"=> NaN
+
+//let isLoggedIn = "";
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1=> true
+//0=>false
+//""=>false
+
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let rendomNumber = 22
+let stringNum = Boolean (rendomNumber)
+console.log(stringNum)
+console.log(typeof stringNum)
+console.log(rendomNumber);
