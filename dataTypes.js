4eaa2a7e964ae84adaf2c39f73eda751edcd4250
om <om@venturingdigitally.com>
2024-07-10 15:56:29 +0530

    first commit

---
+++ dataTypes.js
@@ -0,0 +1,25 @@
+//"use strict";// treats all js code as newer version of js.
+// alert(3 + 3); // we are using node js and in this we can see error and it works on browser.
+// code readability should be high.
+
+//console.log(3 + 3);
+//console.log("ashwin");
+
+
+
+//Primitive data types
+// number - 2to the power of 52
+//bigint - storing the big amount of number
+//string - ""
+//boolean - true/false
+//null - standalone (null came into the object)
+//undefined - value which is not defined
+//symbol - unique
+
+
+//Objects 
+
+// console.log(typeof "Rishi");
+// console.log(typeof 23);
+
+
