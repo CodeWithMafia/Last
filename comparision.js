fe575b5eb17a20b276e42fed7a1214b18a31a391
om <om@venturingdigitally.com>
2024-07-11 19:27:22 +0530

    comaprision datatypes

---
+++ comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+// console.log(2 === 1);
+// console.log(typeof "2" > 1);
+// console.log("02" > 1);
+
+// we are avoiding this kind of comparision
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined != 0);
+
+//we are avoiding this kind of comaparision
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+
+
+// console.log("2" === 2);
