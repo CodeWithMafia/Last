3dbfc1100eed9549016a529af8c9fa74e7035123
om <om@venturingdigitally.com>
2024-07-11 18:45:02 +0530

    Postfix And Prefix

--- conversionOprations.js
+++ conversionOprations.js
@@ -63,3 +63,64 @@
 // console.log(typeof stringNum)
 // console.log(rendomNumber);
 
+//*****************************OPRATIONS***********************************
+
+// let value = 3;
+// let nagValue = -value;
+//console.log(nagValue)
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**4);
+// console.log(2-2);
+// console.log(2%3);
+
+// let str1 = "Welcome";
+// let str2 = " Rishi";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(2 + "2");
+// console.log("3" + 2);
+// console.log(1 + 2 + "3");//(33)if strings at last statement in argument so first statements are added.
+// console.log("1" + 2 + 3);//here string is first so it gives us (123) not adding values.
+
+// console.log(3 + ((4 * 5) % 6));
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+// console.log(num1, num2, num3);
+
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
+
+// let x = 3;
+// let y = x++;
+
+// console.log(`x:${x},y:${y}`);
+
+//POST INCREMENT
+
+// let x = 2;
+// let y = x++;
+// console.log(x); //3
+// console.log(y); //2
+
+// let x2 = 3n;
+// let y2 = x2++;
+// console.log(x2); //4n
+// console.log(y2); //3n
+
+// let x3 = 22;
+// let y3 = x3++;
+// console.log(x3);
+// console.log(y3);
+
+//Prefix Increment
+// let x = 2;
+// const y = ++x;
+// console.log(x);
+// console.log(y);
